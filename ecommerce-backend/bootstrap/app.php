69a082442a79e064b61ef87443b7a521fdba47d8
Lemuel1227 <lemuelellasus1@gmail.com>
2025-04-07 12:01:27 +0800

    I added Cart, Order, Transaction, Authentication for backend

--- ecommerce-backend/bootstrap/app.php
+++ ecommerce-backend/bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsCustomer;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -12,7 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'IsAdmin' => IsAdmin::class,
+            'IsCustomer' => IsCustomer::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
