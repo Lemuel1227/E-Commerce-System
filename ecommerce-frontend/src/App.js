65e9f7a5305390603523eb898842d1313b71ec57
huelgasjames <huelgasjames46@gmail.com>
2025-04-07 12:29:57 +0800

    I created the Admin Panel

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import AdminLayout from './components/admin/AdminLayout';
+import ProductManagement from './components/admin/ProductManagement';
+import TransactionMonitoring from './components/admin/TransactionMonitoring';
+import AdminRoute from './context/AdminRoute';
+
+import NotFound from './components/NotFound'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+
+          <Route element={<AdminRoute />}>              
+            <Route path="/admin" element={<AdminLayout />}>                               
+            <Route index element={<Navigate to="products" replace />} />                               
+              <Route path="products" element={<ProductManagement />} />
+              <Route path="transactions" element={<TransactionMonitoring />} />
+            </Route>
+          </Route>
+
+          <Route path="*" element={<NotFound />} />  
+        </Routes>
+      </Router>
   );
 }
 
